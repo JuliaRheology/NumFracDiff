f50b7ec5cfc40d8a97fb5f805d55141947f355ee
Fozyhh <dandrea0105@gmail.com>
2026-02-23 10:39:26 +0000

    Setup

--- src/NumFracDiff.jl
+++ src/NumFracDiff.jl
@@ -1,5 +1,39 @@
 module NumFracDiff
 
-# Write your package code here.
+    using SpecialFunctions: gamma
+    using FFTW
+    using DSP
+    using Base.Threads
+
+    ######################################################
+
+    # This defines the data type for all arrays, parameters and processing
+    # it is defined as a const to avoid performance penalties.
+    # See julia docs for more info on this.
+    const NumDiffFloat = Float64
+    const NumDiffInt = Int64
+
+    ######################################################
+
+    export generate_weights!, compute!
+
+    # workspace.jl
+    export NumDiffProblem, NumDiffWorkspace, DiffMethod
+    export Caputo, CaputoThreads
+    export GL, GLThreads, GLShortMem, GLShortMemThreads, GLShortMemCorr, GLShortMemCorrThreads
+    export RL, RLThreads, RLShortMemCorr, RLShortMemCorrThreads
+
+    # base.jl
+    export init_workspace, optimal_L!
+
+    export NumDiffFloat, NumDiffInt
+
+    ######################################################
+
+    include("workspace.jl")
+    include("base.jl")
+    include("Caputo.jl")
+    include("RiemannLiouville.jl")
+    include("GrunLetn.jl")
 
 end
