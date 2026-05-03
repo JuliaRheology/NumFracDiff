779b76032c5eee1eb31dd8c0b202791785caf61f
Fozyhh <dandrea0105@gmail.com>
2026-05-03 11:51:56 +0000

    test

--- docs/src/Architecture.jl
+++ docs/src/Architecture.jl
@@ -1,1 +1,28 @@
-#Test?
+# # Architecture
+
+# Normal text
+
+using NumFracDiff
+using Plots
+
+# ## SubTitle
+
+#text
+
+##Commented test code
+foo = timeline(t_start = 0, t_end = 10, step = 0.4)
+foo = strainfunction(foo, t -> sin(t))
+plt = plot(size = (500, 500))
+plot!(plt, foo.t, foo.ϵ,
+      linestyle=:dash,
+      marker=:circle,
+      markersize=8,
+      color=:blue,
+      label="", 
+      framestyle = :box) 
+xlabel!("Time")
+ylabel!("Strain")
+#!nb plt #hide
+
+#md # !!! note "Note"
+#md #     Note
