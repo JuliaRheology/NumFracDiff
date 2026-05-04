35f200e05bfd0fc8a78445e1e1941523fcae7284
Lorenzo Fonnesu <75188388+Fonzy-01@users.noreply.github.com>
2026-05-04 13:45:25 +0200

    test for gl documentation

--- docs/src/gl.jl
+++ docs/src/gl.jl
@@ -1,5 +1,12 @@
 # # Grumwald-Letnikov
 
+### Mathematical Definition
+# The Grünwald-Letnikov fractional derivative of order α of a function f(t) is defined as the limit of the sum:
+# ```math
+# D^{α} f(t) = \lim_{h \to 0} \frac{1}{h^α} \sum_{k=0}^n ω_k^{(α)} f(t - kh)
+# ```
+# where $h$ is the step size and $n=\lfloor{(t-t_0)/h}\rfloor$.
+
 # ## GL
 
 #md # !!! note "Note"
