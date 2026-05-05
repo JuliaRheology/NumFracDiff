fef58663995eeeb8cc54d3ad2fa73d5d033af9a9
Lorenzo Fonnesu <75188388+Fonzy-01@users.noreply.github.com>
2026-05-05 14:40:19 +0200

    small fix

--- docs/src/gl.jl
+++ docs/src/gl.jl
@@ -1,18 +1,26 @@
 # # Grünwald-Letnikov
 
 # ### Mathematical Definition
+
 # The Grünwald-Letnikov fractional derivative of order $α$ of a function $f(t)$ is defined as the limit of the sum:
+
 # $$D^{α} f(t) = \lim_{h \to 0} \frac{1}{h^α} \sum_{k=0}^{n} w_k^{(α)} f(t - kh)$$
+
 # where $h$ is the step size and $n=\lfloor{(t-t_0)/h}\rfloor$. The coefficients $w_k^{α}$ are given by the recursive relation:
+
 # $$w_0^{(α)} = 1 , \quad w_k^{(α)} = w_{k-1}^{(α)} \left(1 - \frac{α + 1}{k}\right)$$
+
 # and can be computed efficiently using the function [`generate_weights!`](@ref).
+
 weights = zeros(NumDiffFloat, L)
 deriv = zeros(NumDiffFloat, L)
 ws = NumDiffWorkspace(weights, deriv)
 generate_weights!(prob.method, prob, ws, L=L)
 
 # ### Numerical Implementation
+
 # Given a sampling time of $\Delta t$ and a vector of data points,  the derivative at index $i$ is calculated as a weighted sum:
+
 # $$D^\alpha f(t_i) \approx \frac{1}{\Delta t^\alpha} \sum_{j=1}^{i} w_{j-1}^{(\alpha)} f(t_{i-j+1})$$
 
 #md # !!! note "Note"
