105ec5a795a9ae35ce8cdcd5e3570e14bdb9d6cc
Lorenzo Fonnesu <75188388+Fonzy-01@users.noreply.github.com>
2026-05-07 15:39:48 +0200

    little fix in optimal_L

--- src/base.jl
+++ src/base.jl
@@ -50,7 +50,7 @@ end
     -------
     Integer memory length (number of time steps).
 """
-function optimal_L!(data::Vector{NumDiffFloat}, prob::NumDiffProblem; tol::Float64 = 1e-2)
+function optimal_L!(data::Vector{NumDiffFloat}, prob::NumDiffProblem; tol::Float64 = 1e-1)
                    
     method = prob.method
 
